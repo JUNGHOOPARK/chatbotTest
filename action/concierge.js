768ef9482308be35fdf8f98dcd56616445f06295
pjh <skwjdg2@gmail.com>
2017-03-23 15:20:26 +0900

    7

--- action/concierge.js
+++ action/concierge.js
@@ -2,42 +2,44 @@ const emotion = require('./emotion');
 
 module.exports.getConciergeExpress = function () {
 
-    return [{
-            "type": "template",
-            "altText": "this is a buttons template",
-            "template": {
-                "type": "buttons",
-                "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
-                "title": "Menu",
-                "text": "어떠한 공간을 인테리어 하시나요?",
-                "actions": [
-                    {
-                        "type": "message",
-                        "label": "상업 공간",
-                        "data": "1"
-                    },
-                    {
-                        "type": "message",
-                        "label": "업무 공간",
-                        "data": "2"
-                    },
-                    {
-                        "type": "message",
-                        "label": "주거 공간",
-                        "data": "3"
-                    },
-                    {
-                        "type": "message",
-                        "label": "문화/종교 공간",
-                        "data": "4"
-                    },
-                    {
-                        "type": "message",
-                        "label": "기타 공간",
-                        "data": "5"
-                    }
+    return [
+                {
+                    "type": "template",
+                    "altText": "this is a buttons template",
+                    "template": {
+                        "type": "buttons",
+                        "thumbnailImageUrl": "https://interiorbrothers.com/img/menu/logo_beta.png",
+                        "title": "Menu",
+                        "text": "어떠한 공간을 인테리어 하시나요?",
+                        "actions": [
+                            {
+                                "type": "message",
+                                "label": "상업 공간",
+                                "data": "1"
+                            },
+                            {
+                                "type": "message",
+                                "label": "업무 공간",
+                                "data": "2"
+                            },
+                            {
+                                "type": "message",
+                                "label": "주거 공간",
+                                "data": "3"
+                            },
+                            {
+                                "type": "message",
+                                "label": "문화/종교 공간",
+                                "data": "4"
+                            },
+                            {
+                                "type": "message",
+                                "label": "기타 공간",
+                                "data": "5"
+                            }
 
-                ]
+                        ]
+                    }
             }
-    }];
+    ];
 };
