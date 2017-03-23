d3a6f0087be83c52a6c0d56ed272b719b04b65ff
pjh <skwjdg2@gmail.com>
2017-03-23 15:56:29 +0900

    update

--- action/concierge.js
+++ action/concierge.js
@@ -5,38 +5,13 @@ module.exports.getConciergeExpress = function (type) {
     if(type === 'start'){
         var result = [
                 {
-                    "type": "template",
-                    "altText": "this is a buttons template",
-                    "template": {
-                        "type": "buttons",
-                        "thumbnailImageUrl": "https://interiorbrothers.com/img/menu/logo_beta.png",
-                        "title": "Menu",
-                        "text": "어떠한 공간을 인테리어 하시나요?",
-                        "actions": [
-                            {
-                                "type": "message",
-                                "label": "상업 공간",
-                                "text": "@category=1"
-                            },
-                            {
-                                "type": "message",
-                                "label": "업무 공간",
-                                "text": "@category=2"
-                            },
-                            {
-                                "type": "message",
-                                "label": "주거 공간",
-                                "text": "@category=3"
-                            },
-                            {
-                                "type": "message",
-                                "label": "문화/종교 공간",
-                                "text": "@category=4"
-                            }
-
-
-                        ]
-                    }
+                    "type": "text",
+                    "text": "어떠한 공간을 인테리어 하시나요?" +
+                    "상업공간" +
+                    "업무공간" +
+                    "주거공간" +
+                    "문화/종교 공간" +
+                    "기타공간"
                 }
             ];
     }else if(type === 'spaces1'){
