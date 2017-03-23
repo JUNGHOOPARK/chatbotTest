11e2ccba15a7f22795dd2205d29eb2f7e66d557a
pjh <skwjdg2@gmail.com>
2017-03-23 14:59:30 +0900

    2

---
+++ action/concierge.js
@@ -0,0 +1,25 @@
+const emotion = require('./emotion');
+
+module.exports.getSureExpress = function () {
+
+    return [{
+        "type": "template",
+        "altText": "this is a confirm template",
+        "template": {
+            "type": "confirm",
+            "text": "브라더스 컨시어지를 시작하시겠습니까?",
+            "actions": [
+                {
+                    "type": "message",
+                    "label": "Yes",
+                    "text": "yes"
+                },
+                {
+                    "type": "message",
+                    "label": "No",
+                    "text": "no"
+                }
+            ]
+        }
+    }];
+};
