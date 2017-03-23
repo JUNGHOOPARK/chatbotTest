ae6b53412e0466158127f78cd135350f2c9f4983
pjh <skwjdg2@gmail.com>
2017-03-23 15:41:27 +0900

    9

--- action/concierge.js
+++ action/concierge.js
@@ -1,8 +1,9 @@
 const emotion = require('./emotion');
 
-module.exports.getConciergeExpress = function () {
+module.exports.getConciergeExpress = function (type) {
 
-    return [
+    if(type === 'start'){
+        var result = [
                 {
                     "type": "template",
                     "altText": "this is a buttons template",
@@ -15,27 +16,67 @@ module.exports.getConciergeExpress = function () {
                             {
                                 "type": "message",
                                 "label": "상업 공간",
-                                "text": "1"
+                                "text": "spaces.category=1"
                             },
                             {
                                 "type": "message",
                                 "label": "업무 공간",
-                                "text": "2"
+                                "text": "spaces.category=2"
                             },
                             {
                                 "type": "message",
                                 "label": "주거 공간",
-                                "text": "3"
+                                "text": "spaces.category=3"
                             },
                             {
                                 "type": "message",
                                 "label": "문화/종교 공간",
-                                "text": "4"
+                                "text": "spaces.category=4"
                             }
 
 
                         ]
                     }
+                }
+            ];
+    }else if(type === 'spaces1'){
+        var result = [
+            {
+                "type": "template",
+                "altText": "this is a buttons template",
+                "template": {
+                    "type": "buttons",
+                    "thumbnailImageUrl": "https://interiorbrothers.com/img/menu/logo_beta.png",
+                    "title": "Menu",
+                    "text": "어떠한 상업 공간을 인테리어 하시나요?",
+                    "actions": [
+                        {
+                            "type": "message",
+                            "label": "요식 식당",
+                            "text": "spaces.category=1"
+                        },
+                        {
+                            "type": "message",
+                            "label": "상업 공간",
+                            "text": "spaces.category=2"
+                        },
+                        {
+                            "type": "message",
+                            "label": "교육 공간",
+                            "text": "spaces.category=3"
+                        },
+                        {
+                            "type": "message",
+                            "label": "의료 공간",
+                            "text": "spaces.category=4"
+                        }
+
+
+                    ]
+                }
             }
-    ];
+        ];
+    }
+
+    return result;
 };
