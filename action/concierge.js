961583ec81101357b6713dab748508f1772b98ea
pjh <skwjdg2@gmail.com>
2017-03-23 16:26:17 +0900

    update

--- action/concierge.js
+++ action/concierge.js
@@ -15,19 +15,10 @@ module.exports.getConciergeExpress = function (type,opt) {
                 }
             ];
     }else if(type === 'spaces1'){
-        var result = null;
-        if(opt === 0){
-            result = [
+        var result = [
                 {
                     "type": "text",
-                    "text": "상업공간,업무공간,주거공간,문화/종교공간,기타공간 중에서 입력해주세요~:]"
-                }
-            ];
-        }else{
-            result = [
-                {
-                    "type": "text",
-                    "text": "어떠한 공간을 인테리어 하시나요?\n\n" +
+                    "text": "어떠한 "+opt+"을 인테리어 하시나요?\n\n" +
                     "1.요식/식당\n" +
                     "2.상업공간\n" +
                     "3.교육공간\n" +
@@ -35,8 +26,6 @@ module.exports.getConciergeExpress = function (type,opt) {
                     "5.운동공간"
                 }
             ];
-        }
-
     }
 
     return result;
