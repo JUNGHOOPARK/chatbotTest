dc63be04e5925a69c2de956a1bb96aa537952033
pjh <skwjdg2@gmail.com>
2017-03-23 16:12:44 +0900

    update

--- action/concierge.js
+++ action/concierge.js
@@ -1,56 +1,42 @@
 const emotion = require('./emotion');
 
-module.exports.getConciergeExpress = function (type) {
+module.exports.getConciergeExpress = function (type,opt) {
 
     if(type === 'start'){
         var result = [
                 {
                     "type": "text",
                     "text": "어떠한 공간을 인테리어 하시나요?\n\n" +
-                    "상업공간\n" +
-                    "업무공간\n" +
-                    "주거공간\n" +
-                    "문화/종교 공간\n" +
-                    "기타공간"
+                    "1.상업공간\n" +
+                    "2.업무공간\n" +
+                    "3.주거공간\n" +
+                    "4.문화/종교 공간\n" +
+                    "5.기타공간"
                 }
             ];
     }else if(type === 'spaces1'){
-        var result = [
-            {
-                "type": "template",
-                "altText": "this is a buttons template",
-                "template": {
-                    "type": "buttons",
-                    "thumbnailImageUrl": "https://interiorbrothers.com/img/menu/logo_beta.png",
-                    "title": "Menu",
-                    "text": "어떠한 상업 공간을 인테리어 하시나요?",
-                    "actions": [
-                        {
-                            "type": "message",
-                            "label": "요식 식당",
-                            "text": "@category=1"
-                        },
-                        {
-                            "type": "message",
-                            "label": "상업 공간",
-                            "text": "@category=2"
-                        },
-                        {
-                            "type": "message",
-                            "label": "교육 공간",
-                            "text": "@category=3"
-                        },
-                        {
-                            "type": "message",
-                            "label": "의료 공간",
-                            "text": "@category=4"
-                        }
-
-
-                    ]
+        var result = null;
+        if(opt === 0){
+            result = [
+                {
+                    "type": "text",
+                    "text": "상업공간,업무공간,주거공간,문화/종교공간,기타공간 중에서 입력해주세요~:]"
                 }
-            }
-        ];
+            ];
+        }else{
+            result = [
+                {
+                    "type": "text",
+                    "text": "어떠한 공간을 인테리어 하시나요?\n\n" +
+                    "1.요식/식당\n" +
+                    "2.상업공간\n" +
+                    "3.교육공간\n" +
+                    "4.의료공간\n" +
+                    "5.운동공간"
+                }
+            ];
+        }
+
     }
 
     return result;
