e389a166c95ed6189b56d8d2bdbe20de9064b4aa
pjh <skwjdg2@gmail.com>
2017-03-24 12:10:55 +0900

    update

--- action/concierge.js
+++ action/concierge.js
@@ -70,8 +70,55 @@ module.exports.getConciergeExpress = function (type,opt) {
 
             }
         ];
-    }
+    }else if(type === 'spaceBranding'){
+        var result = [
+            {
+                "type": "text",
+                "text": "인테리어 하는 공간의 공간 브랜딩을 해줄 수 있는 전문가가 필요하신가요??!\n\n" +
+                "ex) 네,아니오 로 답해주세요. \n"
 
+            }
+        ];
+    }else if(type === 'spaceMeasure'){
+        var result = [
+            {
+                "type": "text",
+                "text": "인테리어 하는 공간의 면적이 어떻게 되나요?\n\n" +
+                "ex) 100m2 || 100평 \n"
+
+            }
+        ];
+    }else if(type === 'budget'){
+        var result = [
+            {
+                "type": "text",
+                "text": "인테리어 하는 공간의 예산은 얼마인가요? \n\n" +
+                "만원 단위로 입력해주세요! \n"+
+                "ex) 100~500 \n"
+
+            }
+        ];
+    }else if(type === 'styles'){
+        var result = [
+            {
+                "type": "text",
+                "text": "인테리어 하는 공간의 예산은 얼마인가요? \n\n" +
+                "1.모던\n" +
+                "2.북유럽\n" +
+                "3.클래식\n" +
+                "4.프로방스&로맨틱\n" +
+                "5.빈티지\n" +
+                "6.한국&아시아\n" +
+                "7.미니멀리즘\n" +
+                "8.인더스트리얼\n" +
+                "9.기타\n" +
+                "10.앤틱\n" +
+                "11.내추럴\n\n" +
+                "ex) 1 || 모던 \n"
+
+            }
+        ];
+    }
 
 
     return result;
@@ -107,5 +154,94 @@ module.exports.getSpaces3 = function (spaces1,spaces2,msg) {
 
         }
     }
+
+};
+
+module.exports.getSpecialtyRange = function (msg) {
+     var result = [],temp = [],arr = [];
+
+    if(msg.indexOf(",") != -1){
+       temp = msg.split(",");
+       for(var i = 0 ; i < temp.length ; i++){
+           if(temp[i] === '1' || temp[i] === '인테리어 디자인'){
+               arr.push('interiorDesign');
+           }else if(temp[i] === '2' || temp[i] === '인테리어 시공'){
+               arr.push('interiorContractor');
+           }else if(temp[i] === '3' || temp[i] === '스타일링'){
+               arr.push('interiorStyling');
+           }
+
+
+       }
+
+        if(arr.length > 1){
+               if(arr[0] === 'interiorDesign' && arr[1] === 'interiorContractor'){
+                    result.push('interiorFull');
+               }else{
+                   for(var i = 0 ; i < arr.length ; i++){
+                       result.push(arr[i]);
+                   }
+               }
+        }else{
+            result.push(arr[0]);
+        }
+    }else{
+        if(msg === '1' || msg === '인테리어 디자인'){
+            result.push('interiorDesign');
+        }else if(msg === '2' || msg === '인테리어 시공'){
+            result.push('interiorContractor');
+        }else if(msg === '3' || msg === '스타일링'){
+            result.push('interiorStyling');
+        }
+
+    }
+
+    return result;
+
+};
+
+module.exports.getMeasure = function (msg) {
+    var result = [];
+
+    if(msg.indexOf("평") != -1){
+        result[0] = msg;
+        result[1] = (msg * 3.3).toFixed(2);
+    }else if(msg.indexOf("m2") != -1){
+        result[0] = (msg / 3.3).toFixed(2);
+        result[1] = msg;
+    }
+
+    return result;
+
+};
+
+module.exports.getStyles = function (msg) {
+    var result = "";
+
+    if(msg.indexOf("1") != -1 || msg === "모던" ){
+        result = "1";
+    }else if(msg.indexOf("2") != -1 || msg === "북유럽" ){
+        result = "2";
+    }else if(msg.indexOf("3") != -1 || msg === "클래식"){
+        result = "3";
+    }else if(msg.indexOf("4") != -1 || msg === "프로방스&로맨틱" ){
+        result = "4";
+    }else if(msg.indexOf("5") != -1 || msg === "빈티지"){
+        result = "5";
+    }else if(msg.indexOf("6") != -1 || msg === "한국&아시아"){
+        result = "6";
+    }else if(msg.indexOf("7") != -1 || msg === "미니멀리즘"){
+        result = "7";
+    }else if(msg.indexOf("8") != -1 || msg === "인더스트리얼"){
+        result = "8";
+    }else if(msg.indexOf("9") != -1 || msg === "기타"){
+        result = "9";
+    }else if(msg.indexOf("10") != -1 || msg === "앤틱"){
+        result = "10";
+    }else if(msg.indexOf("11") != -1 || msg === "내추럴"){
+        result = "11";
+    }
+
+    return result;
 
 };
