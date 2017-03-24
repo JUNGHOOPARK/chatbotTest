321f7af41d80315b7cf28737f960252ed06d3645
pjh <skwjdg2@gmail.com>
2017-03-24 17:04:24 +0900

    update

--- action/concierge.js
+++ action/concierge.js
@@ -241,8 +241,7 @@ module.exports.getConciergeExpress = function (type,opt) {
             };
     }else if(type === 'expertsTest'){
 
-        var result =
-         [
+        var result = [
             {
                 "type": "template",
                 "altText": "this is a buttons template",
@@ -250,33 +249,29 @@ module.exports.getConciergeExpress = function (type,opt) {
                     "type": "buttons",
                     "thumbnailImageUrl": "https://interiorbrothers.com/img/menu/logo_beta.png",
                     "title": "Menu",
-                    "text": "어떠한 공간을 인테리어 하시나요?",
+                    "text": "어떠한 상업 공간을 인테리어 하시나요?",
                     "actions": [
                         {
                             "type": "message",
+                            "label": "요식 식당",
+                            "text": "@category=1"
+                        },
+                        {
+                            "type": "message",
                             "label": "상업 공간",
-                            "data": "1"
+                            "text": "@category=2"
                         },
                         {
                             "type": "message",
-                            "label": "업무 공간",
-                            "data": "2"
+                            "label": "교육 공간",
+                            "text": "@category=3"
                         },
                         {
                             "type": "message",
-                            "label": "주거 공간",
-                            "data": "3"
-                        },
-                        {
-                            "type": "message",
-                            "label": "문화/종교 공간",
-                            "data": "4"
-                        },
-                        {
-                            "type": "message",
-                            "label": "기타 공간",
-                            "data": "5"
+                            "label": "의료 공간",
+                            "text": "@category=4"
                         }
+
 
                     ]
                 }
