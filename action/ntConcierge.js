85944021444b3fcd0efd1fea384f4e84a50c9ccc
pjh <skwjdg2@gmail.com>
2017-04-07 16:50:00 +0900

    1

--- action/ntConcierge.js
+++ action/ntConcierge.js
@@ -451,8 +451,8 @@ module.exports.getConciergeExpress = function (type,body,opt) {
             obj.title =opt[i].businessName+"-"+opt[i].title;
 
             var stringByteLength = obj.title.replace(/[\0-\x7f]|([0-\u07ff]|(.))/g,"$&$1$2").length;
-            console.log(stringByteLength + " Bytes");
-
+            // console.log(stringByteLength + " Bytes");
+            console.log(obj.title.length + " length");
             obj.description = "구분 "+ opt[i].spaceName +"\n시기 "+ opt[i].deadLine +"\n면적 "+(opt[i].size).toFixed(2)+"m²("+(opt[i].size / 3.3).toFixed(2) +" 평)\n예산 "+util.budget[opt[i].budget];
             obj.title = opt[i].businessName+"-"+opt[i].title;
             obj.image = {
